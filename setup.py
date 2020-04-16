e8aa487cd8b4a7cc9ebd1cc047115afadc898252
Petri Savolainen <petri@koodaamo.fi>
2020-04-16 22:49:40 +0300

    remove offending classifier

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
-        'Natural Language :: English',
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
